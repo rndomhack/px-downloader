8053e12e3b79bf605f49834c880cd6a7a4163f9e
rndomhack <rndom.hack@gmail.com>
2016-01-14 22:10:53 +0900

    Update options.html

---
+++ js/options.js
@@ -0,0 +1,52 @@
+/* global chrome */
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    function getOptions() {
+        return new Promise(resolve => {
+            chrome.storage.sync.get([
+                "dir",
+                "file",
+                "page",
+                "ugoiraMode",
+                "gifQuality"
+            ], items => {
+                resolve(items);
+            });
+        });
+    }
+
+    function setOptions(options) {
+        return new Promise(resolve => {
+            chrome.storage.sync.set(options, items => {
+                resolve(items);
+            });
+        });
+    }
+
+    getOptions().then(options => {
+        Object.keys(options).forEach(key => {
+            document.querySelector(`#${key}`).value = options[key];
+        });
+
+        document.querySelector("#dir").addEventListener("change", ev => {
+            setOptions({ dir: ev.currentTarget.value });
+        });
+
+        document.querySelector("#file").addEventListener("change", ev => {
+            setOptions({ file: ev.currentTarget.value });
+        });
+
+        document.querySelector("#page").addEventListener("change", ev => {
+            setOptions({ page: ev.currentTarget.value });
+        });
+
+        document.querySelector("#ugoiraMode").addEventListener("change", ev => {
+            setOptions({ ugoiraMode: ev.currentTarget.value });
+        });
+
+        document.querySelector("#gifQuality").addEventListener("change", ev => {
+            setOptions({ gifQuality: parseInt(ev.currentTarget.value, 10) });
+        });
+    });
+});
